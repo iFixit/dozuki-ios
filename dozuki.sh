758dc73896076af8a5c56906c14d4d39fe1b8c0d
Bob Pascazio <bob@managedapps.co>
2017-07-14 05:46:26 +0700

    Everything for zeal.

--- dozuki.sh
+++ dozuki.sh
@@ -15,11 +15,11 @@ elif [ "$1" == "make" ]; then
 elif [ "$1" == "zeal" ]; then
    echo "Zeal"
    cp Graphics/Sites/Zeal/*png .
-   sed -i '.bak' -e 's/com.ifixit.ifixit/com.dozuki.zeal/g' iFixit-Info.plist
-   sed -i '.bak' -e 's/iFixit/Zeal Support/g' iFixit-Info.plist
-   sed -i '.bak' -e 's/>ifixit</>zeal</g' iFixit-Info.plist
+   sed -i '.bak' -e 's/com.dozuki.dozuki/com.dozuki.zeal/g' iFixit-Info.plist
+   sed -i '.bak' -e 's/Dozuki/Zeal Support/g' iFixit-Info.plist
+   sed -i '.bak' -e 's/>dozuki</>zeal</g' iFixit-Info.plist
    sed -i '.bak' -e 's/\[Config currentConfig\].dozuki = YES;/\[Config currentConfig\].dozuki = NO;/' Classes/iFixitAppDelegate.m
-   sed -i '.bak' -e 's/\[Config currentConfig\].site = ConfigIFixit;/\[Config currentConfig\].site = ConfigZeal;/' Classes/iFixitAppDelegate.m
+   sed -i '.bak' -e 's/\[Config currentConfig\].site = ConfigDozuki;/\[Config currentConfig\].site = ConfigZeal;/' Classes/iFixitAppDelegate.m
    sed -i '.bak' -e 's/ifixit/zeal/g' Classes/iFixitAPI.m
 elif [ "$1" == "mjtrim" ]; then
    echo "Mjtrim"
