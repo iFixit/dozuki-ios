f5e322409afdda9aeb5cf2808ff2e0f5755bd57f
Bob Pascazio <bob@managedapps.co>
2018-05-09 16:19:58 -0700

    Fixed indentation.

--- Classes/iPhoneDeviceViewController.m
+++ Classes/iPhoneDeviceViewController.m
@@ -26,148 +26,148 @@
 @synthesize wikis=_wikis;
 
 - (id)initWithTopic:(NSString *)topic {
-    if ((self = [super initWithNibName:nil bundle:nil])) {
-        self.topic = topic;
-         self.guides = [NSArray array];
-         self.wikis = [NSArray array];
-         self.cats = [NSArray array];
-        
-        if (!topic)
-            self.title = NSLocalizedString(@"Guides", nil);
-        
-        [self getGuides];
-    }
-    return self;
+     if ((self = [super initWithNibName:nil bundle:nil])) {
+          self.topic = topic;
+          self.guides = [NSArray array];
+          self.wikis = [NSArray array];
+          self.cats = [NSArray array];
+          
+          if (!topic)
+               self.title = NSLocalizedString(@"Guides", nil);
+          
+          [self getGuides];
+     }
+     return self;
 }
 
 #pragma mark - View lifecycle
 
 - (void)viewWillAppear:(BOOL)animated {
-//    if (self.currentCategory)
-//        self.navigationItem.title = self.currentCategory;
+     //    if (self.currentCategory)
+     //        self.navigationItem.title = self.currentCategory;
 }
 
 - (void)showRefreshButton {
-    // Show a refresh button in the navBar.
-    UIBarButtonItem *refreshButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh
-                                                                                   target:self
-                                                                                   action:@selector(getGuides)];
-    self.navigationItem.rightBarButtonItem = refreshButton;
-    [refreshButton release];   
+     // Show a refresh button in the navBar.
+     UIBarButtonItem *refreshButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh
+                                                                                    target:self
+                                                                                    action:@selector(getGuides)];
+     self.navigationItem.rightBarButtonItem = refreshButton;
+     [refreshButton release];
 }
 
 - (void)showLoading {
-    loading = YES;
-    UIView *container = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 28.0f, 20.0f)];
-    UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 20.0f, 20.0f)];
-    spinner.activityIndicatorViewStyle = ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) ?
-    UIActivityIndicatorViewStyleGray : UIActivityIndicatorViewStyleWhite;
-    [container addSubview:spinner];
-    [spinner startAnimating];
-    [spinner release];
-    
-    UIBarButtonItem *button = [[UIBarButtonItem alloc] initWithCustomView:container];
-    self.navigationItem.rightBarButtonItem = button;
-    [container release];
-    [button release];
+     loading = YES;
+     UIView *container = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 28.0f, 20.0f)];
+     UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 20.0f, 20.0f)];
+     spinner.activityIndicatorViewStyle = ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) ?
+     UIActivityIndicatorViewStyleGray : UIActivityIndicatorViewStyleWhite;
+     [container addSubview:spinner];
+     [spinner startAnimating];
+     [spinner release];
+     
+     UIBarButtonItem *button = [[UIBarButtonItem alloc] initWithCustomView:container];
+     self.navigationItem.rightBarButtonItem = button;
+     [container release];
+     [button release];
 }
 - (void)hideLoading {
-    loading = NO;
-    self.navigationItem.rightBarButtonItem = nil;
-    [self.listViewController showFavoritesButton:self];
+     loading = NO;
+     self.navigationItem.rightBarButtonItem = nil;
+     [self.listViewController showFavoritesButton:self];
 }
 
 - (void)getGuides {
-    if (!loading) {
-        loading = YES;
-        [self showLoading];
-        
-        if (self.topic)
-            [[iFixitAPI sharedInstance] getCategory:self.topic forObject:self withSelector:@selector(gotCategory:)];
-        else
-            [[iFixitAPI sharedInstance] getGuides:nil forObject:self withSelector:@selector(gotGuides:)];
-    }
+     if (!loading) {
+          loading = YES;
+          [self showLoading];
+          
+          if (self.topic)
+               [[iFixitAPI sharedInstance] getCategory:self.topic forObject:self withSelector:@selector(gotCategory:)];
+          else
+               [[iFixitAPI sharedInstance] getGuides:nil forObject:self withSelector:@selector(gotGuides:)];
+     }
 }
 
 - (void)gotGuides:(NSArray *)guides {
-    
-    if (!guides) {
-        [iFixitAPI displayConnectionErrorAlert];
-        [self showRefreshButton];
-    }
-    
-    self.guides = guides;
-    [self.tableView reloadData];
-    [self hideLoading];
+     
+     if (!guides) {
+          [iFixitAPI displayConnectionErrorAlert];
+          [self showRefreshButton];
+     }
+     
+     self.guides = guides;
+     [self.tableView reloadData];
+     [self hideLoading];
 }
 - (void)gotCategory:(NSDictionary *)data {
-    if (!data) {
-        [iFixitAPI displayLoggedOutErrorAlert:self];
-        [self showRefreshButton];
-        return;
-    }
-    
-    
+     if (!data) {
+          [iFixitAPI displayLoggedOutErrorAlert:self];
+          [self showRefreshButton];
+          return;
+     }
+     
+     
      self.guides = [data arrayForKey:@"guides"];
      self.wikis = [data arrayForKey:@"related_wikis"];
      self.cats = [data arrayForKey:@"children"];
-    [self.tableView reloadData];
-    [self hideLoading];
-    
-    if (!self.guides)
-        [self showRefreshButton];
+     [self.tableView reloadData];
+     [self hideLoading];
+     
+     if (!self.guides)
+          [self showRefreshButton];
 }
 
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
-    [self.navigationController popViewControllerAnimated:YES];
+     [self.navigationController popViewControllerAnimated:YES];
 }
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-
-    // Grab reference to listViewController
-    self.listViewController = (ListViewController*)self.navigationController;
-    
-    // Make room for the toolbar
-    [self willRotateToInterfaceOrientation:self.interfaceOrientation duration:0];
-
-    if (loading)
-        [self showLoading];
-    
-    // Show the Dozuki sites select button if needed.
-    if ([Config currentConfig].dozuki && !self.topic) {
-        UIImage *icon = [UIImage imageNamed:@"backtosites.png"];
-        UIBarButtonItem *button = [[UIBarButtonItem alloc] initWithImage:icon style:UIBarButtonItemStyleBordered
-                                                                  target:[[UIApplication sharedApplication] delegate]
-                                                                  action:@selector(showDozukiSplash)];
-        self.navigationItem.leftBarButtonItem = button;
-        [button release];
-    }
+     [super viewDidLoad];
+     
+     // Grab reference to listViewController
+     self.listViewController = (ListViewController*)self.navigationController;
+     
+     // Make room for the toolbar
+     [self willRotateToInterfaceOrientation:self.interfaceOrientation duration:0];
+     
+     if (loading)
+          [self showLoading];
+     
+     // Show the Dozuki sites select button if needed.
+     if ([Config currentConfig].dozuki && !self.topic) {
+          UIImage *icon = [UIImage imageNamed:@"backtosites.png"];
+          UIBarButtonItem *button = [[UIBarButtonItem alloc] initWithImage:icon style:UIBarButtonItemStyleBordered
+                                                                    target:[[UIApplication sharedApplication] delegate]
+                                                                    action:@selector(showDozukiSplash)];
+          self.navigationItem.leftBarButtonItem = button;
+          [button release];
+     }
      
      [self.tableView registerNib:[UINib nibWithNibName:@"WikiCell" bundle:nil] forCellReuseIdentifier:@"WikiCell"];
 }
 
 - (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
-    // Make room for the toolbar
-    if (UIInterfaceOrientationIsPortrait(toInterfaceOrientation)) {
-        self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 44, 0);
-        self.tableView.scrollIndicatorInsets = UIEdgeInsetsMake(0, 0, 44, 0);
-    }
-    else {
-        self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 32, 0);
-        self.tableView.scrollIndicatorInsets = UIEdgeInsetsMake(0, 0, 32, 0);
-    }
+     // Make room for the toolbar
+     if (UIInterfaceOrientationIsPortrait(toInterfaceOrientation)) {
+          self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 44, 0);
+          self.tableView.scrollIndicatorInsets = UIEdgeInsetsMake(0, 0, 44, 0);
+     }
+     else {
+          self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 32, 0);
+          self.tableView.scrollIndicatorInsets = UIEdgeInsetsMake(0, 0, 32, 0);
+     }
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+     // Return YES for supported orientations
+     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    // Return the number of sections.
+     // Return the number of sections.
      int amount = 1;
      if (self.wikis != nil && [self.wikis count] > 0) {
           amount++;
@@ -175,48 +175,48 @@
      if (self.cats != nil && [self.cats count] > 0) {
           amount++;
      }
-    return 3;
+     return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    // Return the number of rows in the section.
+     // Return the number of rows in the section.
      if (section == 0) {
           return [self.guides count];
      } else if (section == 1) {
           return [self.cats count];
      } else if (section == 2) {
           return [self.wikis count];
-    }
-
+     }
+     
      return 0;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *CellIdentifier = @"GuideCell";
-    
-     if (indexPath.section == 0) {
+     static NSString *CellIdentifier = @"GuideCell";
      
-    GuideCell *cell = (GuideCell*)[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell = [[[GuideCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
-    }
-    
-    // Configure the cell...
-    NSString *title = [self.guides[indexPath.row][@"title"] isEqual:@""] ? NSLocalizedString(@"Untitled", nil) : self.guides[indexPath.row][@"title"];
-    
-
-    title = [title stringByReplacingOccurrencesOfString:@"&amp;" withString:@"&"];
-    title = [title stringByReplacingOccurrencesOfString:@"&quot;" withString:@"\""];
-    title = [title stringByReplacingOccurrencesOfString:@"<wbr />" withString:@""];
-    
-    cell.textLabel.text = title;
-    
-    NSDictionary *imageData = self.guides[indexPath.row][@"image"];
-    NSString *thumbnailURL = [imageData isEqual:[NSNull null]] ? nil : imageData[@"thumbnail"];
-    
-    [cell.imageView setImageWithURL:[NSURL URLWithString:thumbnailURL] placeholderImage:[UIImage imageNamed:@"WaitImage.png"]];
-    
-    return cell;
+     if (indexPath.section == 0) {
+          
+          GuideCell *cell = (GuideCell*)[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+          if (cell == nil) {
+               cell = [[[GuideCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+          }
+          
+          // Configure the cell...
+          NSString *title = [self.guides[indexPath.row][@"title"] isEqual:@""] ? NSLocalizedString(@"Untitled", nil) : self.guides[indexPath.row][@"title"];
+          
+          
+          title = [title stringByReplacingOccurrencesOfString:@"&amp;" withString:@"&"];
+          title = [title stringByReplacingOccurrencesOfString:@"&quot;" withString:@"\""];
+          title = [title stringByReplacingOccurrencesOfString:@"<wbr />" withString:@""];
+          
+          cell.textLabel.text = title;
+          
+          NSDictionary *imageData = self.guides[indexPath.row][@"image"];
+          NSString *thumbnailURL = [imageData isEqual:[NSNull null]] ? nil : imageData[@"thumbnail"];
+          
+          [cell.imageView setImageWithURL:[NSURL URLWithString:thumbnailURL] placeholderImage:[UIImage imageNamed:@"WaitImage.png"]];
+          
+          return cell;
      } else if (indexPath.section == 1) {
           
           GuideCell *cell = (GuideCell*)[tableView dequeueReusableCellWithIdentifier:@"GuideCell"];
@@ -230,20 +230,20 @@
           [cell.imageView setImageWithURL:[NSURL URLWithString:thumbnailURL] placeholderImage:[UIImage imageNamed:@"WaitImage.png"]];
           [cell setNeedsLayout];
           return cell;
-               } else {
-                    
-                    GuideCell *cell = (GuideCell*)[tableView dequeueReusableCellWithIdentifier:@"GuideCell"];
-                    if (cell == nil) {
-                         cell = [[GuideCell alloc] initWithStyle:UITableViewStylePlain reuseIdentifier:@"GuideCell"];
-                    }
-                    NSString *title = [self.wikis[indexPath.row][@"title"] isEqual:@""] ? NSLocalizedString(@"Untitled", nil) : self.wikis[indexPath.row][@"title"];
-                    [cell.textLabel setText:title];
-                    NSDictionary *imageData = self.wikis[indexPath.row][@"image"];
-                    NSString *thumbnailURL = [imageData isEqual:[NSNull null]] ? nil : imageData[@"medium"];
-                    [cell.imageView setImageWithURL:[NSURL URLWithString:thumbnailURL] placeholderImage:[UIImage imageNamed:@"WaitImage.png"]];
-                    [cell setNeedsLayout];
-                    return cell;
-
+     } else {
+          
+          GuideCell *cell = (GuideCell*)[tableView dequeueReusableCellWithIdentifier:@"GuideCell"];
+          if (cell == nil) {
+               cell = [[GuideCell alloc] initWithStyle:UITableViewStylePlain reuseIdentifier:@"GuideCell"];
+          }
+          NSString *title = [self.wikis[indexPath.row][@"title"] isEqual:@""] ? NSLocalizedString(@"Untitled", nil) : self.wikis[indexPath.row][@"title"];
+          [cell.textLabel setText:title];
+          NSDictionary *imageData = self.wikis[indexPath.row][@"image"];
+          NSString *thumbnailURL = [imageData isEqual:[NSNull null]] ? nil : imageData[@"medium"];
+          [cell.imageView setImageWithURL:[NSURL URLWithString:thumbnailURL] placeholderImage:[UIImage imageNamed:@"WaitImage.png"]];
+          [cell setNeedsLayout];
+          return cell;
+          
      }
      return nil;
 }
@@ -331,9 +331,9 @@ heightForHeaderInSection:(NSInteger)section {
 }
 
 - (void)dealloc {
-    [_topic release];
-    [_guides release];
-    [super dealloc];
+     [_topic release];
+     [_guides release];
+     [super dealloc];
 }
 
 @end
