04a59f0b0f61fe8bf7c73ccd3256b652f17a529e
Bob Pascazio <bob@managedapps.co>
2019-01-29 07:07:53 +0700

    Reworked hypertherm variant.

---
+++ variants/hypertherm/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo making hypertherm
+
+cp -v iFixit-Info.plist ../../iFixit-Info.plist
+cp -v Podfile ../../Podfile
+cp -v icon* ../../
+cp -v guids.png ../../
+cp -v GoogleService-Info.plist ../../
+cp -v Default* ../../
+cp -v icon* ../../
+mkdir ../../iFixit/Images.xcassets/AppIcon-4.appiconset
+cp -v AppIcon-3.appiconset/* ../../iFixit/Images.xcassets/AppIcon-4.appiconset
+cp -v Config.h ../../Classes/Config.h
+cp -v Config.m ../../Classes/Config.m
+cp -v iFixitAppDelegate.m ../../Classes/iFixitAppDelegate.m
+cp -v Hypertherm.xcodeproj/* ../../Hypertherm.xcodeproj 
+cd ../..
+pod install
+open Hypertherm.xcworkspace
